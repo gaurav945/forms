5ecc26fdac094d1f1a7e2d5a826d92897e587903
Gaurav <oneknight2dark@gmail.com>
2017-01-22 21:04:55 +0530

    some changes in settings

--- forms/settings.py
+++ forms/settings.py
@@ -115,11 +115,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
